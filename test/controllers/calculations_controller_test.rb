0c74bbab58f43cb57354f28e830cd1ba9940afdd
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-08-15 10:48:12 -0300

    Test the output of Calculations controller

--- test/controllers/calculations_controller_test.rb
+++ test/controllers/calculations_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CalculationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "all operations" do
+    Calculation.operations.keys.each do |op|
+      post '/calculations', xhr: true, params: {
+        left_input: 3,
+        right_input: 2,
+        operation: op
+      }
+      assert_response :success
+    end
+  end
+
+  test 'division by zero' do
+    [0, 1].each do |left_input|
+      post '/calculations', xhr: true, params: {
+        left_input: left_input,
+        right_input: 0,
+        operation: 'division_op'
+      }
+      assert_response :success
+      assert_equal "error\n", response.body
+    end
+  end
 end
